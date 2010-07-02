22b662e65cfdc587a203ce8836dc43c8c59a45f0
Severnaya 99 <severnaya@ilpescecane.it>
2010-07-02 17:43:56 +0200

    Sito originale + Lingua italiana

---
+++ install/language/italian/support.php
@@ -0,0 +1,27 @@
+<?php
+// $Id$
+// _LANGCODE: it
+// _CHARSET : UTF-8
+// Translator: Ianez - Xoops Italia Staff
+
+//$supports = array();
+
+// Add extra support sites, use corresponding language folder name as key, for instance:
+$supports["italian"] = array(
+    "url"   => "http://www.xoopsitalia.org/",
+    "title" => "Xoops Italia (Supporto italiano)",);
+
+
+// Support site for English
+$supports["english"] = array(
+    "url"   => "http://www.xoops.org/",
+    "title" => "Sito ufficiale (in inglese)",
+);
+
+/*
+$supports["french"] = array(
+    "url"   => "http://www.frxoops.org/",
+    "title" => "Support francophone"
+);
+*/
+?>
