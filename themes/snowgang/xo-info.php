359480cadbd6d13bddbe319dc9b2cf106a8737d1
Severnaya 99 <severnaya@ilpescecane.it>
2010-07-02 19:03:12 +0200

    Aggiunti tutti i moduli

---
+++ themes/snowgang/xo-info.php
@@ -0,0 +1,43 @@
+<?php
+/*
+ You may not change or alter any portion of this comment or credits
+ of supporting developers from this source code or any supporting source code 
+ which is considered copyrighted (c) material of the original comment or credit authors.
+ 
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+*/
+
+/**
+ * XOOPS default theme configuration file
+ *
+ * Not used yet, for forward compat
+ *
+ * @copyright   The Xoops project http://www.xoops.org/
+ * @license     http://www.fsf.org/copyleft/gpl.html GNU public license
+ * @author      Taiwen Jiang <phppp@users.sourceforge.net>
+ * @since       2.3
+ * @version     $Id: xo-info.php 2793 2009-02-10 19:49:25Z dugris $
+ */
+
+
+return array(
+    // Types of language constants
+    "languages"         => array("main", "admin"),
+
+    "copyright"         => "&copy; burning, <http://www.frxoops.org>",
+    "name"              => "Zetagenesis 2 Yellow",
+    "description"       => "XOOPS 2.0.1x and 2.3.x, 1/2 or 3 columns, full screen",
+    "version"           => "2",
+    "format"            => "XOOPS Zetagenesis",
+    "author"            => "Burning - frxoops.org",
+    "demo"              => "http://www.frxoops.org",
+    "url"               => "http://www.frxoops.org",
+    "download"          => "http://www.frxoops.org",
+    "W3C"               => "Xhtml 1.0 Transitional - Css 1/2/3",
+    "licence"           => "GPL",
+    "thumbnail"         => "shot.gif",
+    "screenshot"        => "zeta2_yellow.png",
+);
+?>
